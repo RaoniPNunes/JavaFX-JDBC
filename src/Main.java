0fed65dc8d405b414f5a102be3fa7cbbda6c6894
Raoni Padilha Nunes <79607898+RaoniPNunes@users.noreply.github.com>
2021-05-20 11:05:43 -0300

    Alterando a main view adicionando menubar e configurando os menus

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.io.IOException;
 import javafx.application.Application;
@@ -12,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -25,11 +22,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
        try{
            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-           Parent parent = loader.load();
+           ScrollPane scrollPane = loader.load();
            
-           Scene mainScene = new Scene(parent);
+           scrollPane.setFitToWidth(true);
+           scrollPane.setFitToHeight(true);
+           
+           Scene mainScene = new Scene(scrollPane);
            primaryStage.setScene(mainScene);
-           primaryStage.setTitle("Sample Javafx Aplication");
+           primaryStage.setTitle("Registrador");
            primaryStage.show();
        }
        catch(IOException e){
@@ -37,9 +37,7 @@ public class Main extends Application {
        }
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
